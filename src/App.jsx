3da59eb01373ef12c1fc318623e9bbd1144a49c5
pathikroy86 <pathik.roy86@gmail.com>
2026-04-03 22:59:17 +0600

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
 
       <h1>React+Vive</h1>
+      <Navbar></Navbar>
     </>
   )
 }
