0b15a8e6ee028244b0a67416f28ad9bcbdee4e4c
pathikroy86 <pathik.roy86@gmail.com>
2026-04-05 22:38:29 +0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 
 import './App.css'
+import FormAction from './Components/FormAction/FormAction'
 import Navbar from './Components/Navbar/Navbar'
+import SimpleForm from './Components/React-from/SimpleForm'
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
+      <div className='flex justify-center text-center'>
+        <SimpleForm></SimpleForm>
+      </div>
+      <div className='flex justify-center text-center mt-10'>
+        <FormAction></FormAction>
+      </div>
     </>
   )
 }
